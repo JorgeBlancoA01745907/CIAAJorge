307c5778e4fea9efd2d52422f77311425e012e6e
JorgeBlancoA01745907 <99824154+JorgeBlancoA01745907@users.noreply.github.com>
2023-08-30 20:58:21 -0600

    servidor

--- servidor.py
+++ servidor.py
@@ -1,1 +1,10 @@
-# kqaoqka
+#Generar seervidor
+from flask import Flask, render_template
+servidorWeb = Flask(__name__)
+
+@servidorWeb.route("/holamundo",methods=['GET'])
+def holamundo():
+        return render_template('pagina1.html')
+
+if __name__ == '__main__':
+        servidorWeb.run(debug=False,host='0.0.0.0',port='8080')
