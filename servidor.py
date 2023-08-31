bc2079fc05369afba0f034fda3b60c1446d3faed
JorgeBlancoA01745907 <99824154+JorgeBlancoA01745907@users.noreply.github.com>
2023-08-31 17:33:24 -0600

    clase

--- servidor.py
+++ servidor.py
@@ -1,8 +1,25 @@
 #Generar seervidor
-from flask import Flask, render_template
+from flask import Flask, render_template,jsonify,request
+import numpy as np
+from joblib import load
+import os
+
+#Cargar el modelo
+dt=load('dt1.joblib')
+
+#Servidor (backend)
 servidorWeb = Flask(__name__)
 
 @servidorWeb.route("/holamundo",methods=['GET'])
+def formulario():
+        return render_template('pagina1.html')
+
+#Envio de datos a través de JSON
+@servidorWeb.route('/modelo',methods=['POST'])
+def modeloPrediccion():
+       contenido = request.json
+       print(contenido)
+       return jsonify({'resultado':'Hola'})
 def holamundo():
         return render_template('pagina1.html')
 
